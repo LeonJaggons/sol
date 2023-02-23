28a24b6321b9d69840ea205575150386260e375b
LeonJaggons <leonojaggon@gmail.com>
2023-02-23 09:37:14 -0500

    Add user login to auth <-> app

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { isEmpty } from "lodash";
 import { NativeBaseProvider } from "native-base";
+import { useEffect, useState } from "react";
 import { LogBox } from "react-native";
+import { Provider, useSelector } from "react-redux";
 import AppStack from "./src/components/app/AppStack";
 import AuthStack from "./src/components/auth/AuthStack";
 import "./src/firebase/firebase-init.js";
+import store from "./src/redux/store";
 
 LogBox.ignoreLogs(["AsyncStorage..."]);
+
 export default function Application() {
     return (
         <NativeBaseProvider>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </NativeBaseProvider>
     );
 }
 
 function App() {
+    const user = useSelector((state) => state.app.user);
+    const [isApp, setIsApp] = useState(false);
+    const handleAccessApp = () => {
+        setIsApp(user && !isEmpty(user));
+    };
+
+    useEffect(() => {
+        handleAccessApp();
+        console.log(user);
+    }, [user]);
     return (
         <NavigationContainer>
-            {false ? <AppStack /> : <AuthStack />}
+            {isApp ? <AppStack /> : <AuthStack />}
         </NavigationContainer>
     );
 }
