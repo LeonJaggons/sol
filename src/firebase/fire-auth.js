bdeb270f919b82cb729ca841373803accd1c0d6b
LeonJaggons <leonojaggon@gmail.com>
2023-02-23 09:37:23 -0500

    Add util auth functons

--- src/firebase/fire-auth.js
+++ src/firebase/fire-auth.js
@@ -1,4 +1,7 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 import {
     collection,
     doc,
@@ -7,6 +10,7 @@ import {
     Timestamp,
     updateDoc,
 } from "firebase/firestore";
+import store from "../redux/store.js";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Fire from "./firebase-init";
 
@@ -17,6 +21,7 @@ export const createNewAccount = async (userInfo) => {
         userInfo.password
     );
     delete userInfo["password"];
+    delete userInfo["confirmPassword"];
     const userID = userCred.user.uid;
     const imgURL = await uploadProfileImg(userID, userInfo.image);
     userInfo = {
@@ -30,6 +35,11 @@ export const createNewAccount = async (userInfo) => {
     await setDoc(userDocRef, {
         ...userInfo,
     });
+    store.dispatch({
+        type: "SET",
+        attr: "user",
+        payload: { ...userInfo },
+    });
 };
 
 export const uploadProfileImg = async (userID, img) => {
@@ -39,4 +49,27 @@ export const uploadProfileImg = async (userID, img) => {
     const res = await uploadBytes(uiRef, imgBlob);
     const dlURL = await getDownloadURL(res.ref);
     return dlURL;
+};
+
+export const attemptSignIn = async (userInfo) => {
+    const res = await signInWithEmailAndPassword(
+        Fire.auth,
+        userInfo.email,
+        userInfo.password
+    );
+    if (res.user) {
+        const userID = res.user.uid;
+        const userData = await getUserData(userID);
+        store.dispatch({
+            type: "SET",
+            attr: "user",
+            payload: { ...userData },
+        });
+    }
+};
+
+export const getUserData = async (userID) => {
+    const userDoc = doc(Fire.store, "users", userID);
+    const userData = (await getDoc(userDoc)).data();
+    return { ...userData, created: userData.created.toDate() };
 };
