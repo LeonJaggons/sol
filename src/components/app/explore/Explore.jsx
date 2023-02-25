cdb30ed453cea41847016f3ea51183fe0947f9c1
Leon Jaggon <leonojaggon@gmail.com>
2023-02-25 12:58:35 -0500

    Add item display, item reload on focus

--- src/components/app/explore/Explore.jsx
+++ src/components/app/explore/Explore.jsx
@@ -1,9 +1,23 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ExploreMain from "./ExploreMain";
 
 const Explore = () => {
+    const dispatch = useDispatch();
     const Stack = createStackNavigator();
+    useEffect(() => {
+        dispatch({
+            type: "SET",
+            attr: "postCategory",
+            payload: null,
+        });
+        dispatch({
+            type: "SET",
+            attr: "postDetails",
+            payload: {},
+        });
+    }, []);
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name={"ExploreMain"} component={ExploreMain} />
--- src/components/app/explore/ExploreMain.jsx
+++ src/components/app/explore/ExploreMain.jsx
@@ -1,14 +1,68 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Box, HStack, Input } from "native-base";
+import React, { useEffect, useState } from "react";
+import {
+    Box,
+    Center,
+    Heading,
+    HStack,
+    Image,
+    Input,
+    SimpleGrid,
+    Spinner,
+    VStack,
+} from "native-base";
 import SolInput from "../../util/SolInput";
+import { getAllItems } from "../../../firebase/fire-store";
+import { useIsFocused } from "@react-navigation/native";
 
 const ExploreMain = () => {
     return (
-        <Box safeArea p={4}>
+        <Box safeArea p={4} flex={1}>
             <SolInput placeholder={"Search Sol items..."} size={"lg"} />
+            <ExploreGallery />
         </Box>
     );
 };
 
+const ExploreGallery = () => {
+    const [items, setItems] = useState();
+    const isFocused = useIsFocused();
+    const loadItems = async () => {
+        setItems(null);
+        const newItems = await getAllItems();
+        setItems([...newItems]);
+    };
+
+    useEffect(() => {
+        isFocused && loadItems();
+    }, [isFocused]);
+
+    return (
+        <Box flex={1}>
+            {items ? (
+                <HStack columns={3}>
+                    {items.map((item) => (
+                        <Item item={item} />
+                    ))}
+                </HStack>
+            ) : (
+                <Center flex={1}>
+                    <Spinner size={"lg"} color={"muted.800"} />
+                </Center>
+            )}
+        </Box>
+    );
+};
+
+const Item = ({ item }) => {
+    return (
+        <Box w={"1/3"} p={1}>
+            <Image
+                source={{ uri: item.imgs[0] }}
+                w={"full"}
+                style={{ aspectRatio: 1 }}
+            />
+        </Box>
+    );
+};
 export default ExploreMain;
