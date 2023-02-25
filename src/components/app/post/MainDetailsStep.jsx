3d35ef13867139dc25fbdf9c5e6974a7d7b843e5
LeonJaggons <leonojaggon@gmail.com>
2023-02-25 11:16:22 -0500

    Add scroll, city/zip determination

--- src/components/app/post/MainDetailsStep.jsx
+++ src/components/app/post/MainDetailsStep.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import PostStep from "./PostStep";
-import { Box, Heading, HStack, Icon, Select, VStack } from "native-base";
+import {
+    Text,
+    Box,
+    Button,
+    Heading,
+    HStack,
+    Icon,
+    ScrollView,
+    Select,
+    Spinner,
+    VStack,
+    Center,
+    Tag,
+} from "native-base";
 import SolInput, { SolTextArea } from "../../util/SolInput";
 import CategorySelect from "../../util/CategorySelect";
 import { useDispatch, useSelector } from "react-redux";
 import { Ionicons } from "react-native-vector-icons";
+import { find } from "lodash";
+const cities = require("../../util/ZipCities.json");
 const MainDetailsStep = () => {
     const postDetails = useSelector((state) => state.post.postDetails);
     const [canContinue, setCanContinue] = useState(false);
@@ -21,7 +35,9 @@ const MainDetailsStep = () => {
     }, [postDetails]);
     return (
         <PostStep title={"Main Details"} canContinue={canContinue} step={1}>
-            <MainDetailForm />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <MainDetailForm />
+            </ScrollView>
         </PostStep>
     );
 };
@@ -35,12 +51,14 @@ const MainDetailForm = () => {
     const setDescription = (val) => setDetail("description", val);
     const setCategory = (val) => setDetail("category", val);
     const setCondition = (val) => setDetail("condition", val);
+    const setLocation = (val) => setDetail("location", val);
+    const postDetails = useSelector((state) => state.post.postDetails);
 
     useEffect(() => {
         dispatch({
             type: "SET",
             attr: "postDetails",
-            payload: { ...details },
+            payload: { ...postDetails, ...details },
         });
     }, [details]);
 
@@ -53,7 +71,7 @@ const MainDetailForm = () => {
             <VStack space={1}>
                 <Heading size={"sm"}>Title</Heading>
                 <SolInput
-                    fontSize={24}
+                    fontSize={32}
                     fontWeight={700}
                     onChangeText={setTitle}
                 />
@@ -70,24 +88,29 @@ const MainDetailForm = () => {
                 <Heading size={"sm"}>Condition</Heading>
                 <ConditionSelect submit={setCondition} />
             </VStack>
+            <VStack space={1}>
+                <Heading size={"sm"}>Zipcode</Heading>
+                <ZipCodeInput submit={setLocation} />
+            </VStack>
         </VStack>
     );
 };
 
 const ConditionSelect = ({ submit }) => {
     return (
-        <HStack p={1} alignItems={"center"}>
+        <HStack alignItems={"center"} bg={"muted.200"} p={2}>
             <Icon
                 as={Ionicons}
                 name={"chevron-down"}
                 mr={2}
                 color={"muted.600"}
+                size={"18px"}
             />
             <Select
                 onValueChange={submit}
                 flex={1}
-                fontSize={14}
-                fontWeight={400}
+                fontSize={16}
+                fontWeight={"medium"}
                 p={"0px"}
                 placeholder={"Select condition"}
                 color={"black"}
@@ -101,6 +124,88 @@ const ConditionSelect = ({ submit }) => {
                 <Select.Item label={"Broken/For parts"} value={"BROKEN"} />
             </Select>
         </HStack>
+    );
+};
+
+const ZipCodeInput = ({ submit }) => {
+    const [currZip, setCurrZip] = useState();
+    const [city, setCity] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const updateCity = () => {
+        setLoading(true);
+        const foundCity = find(
+            cities,
+            (o) => o.zip_code.toString() === currZip
+        );
+        setCity(foundCity);
+        submit(foundCity);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
+    };
+
+    useEffect(() => {
+        updateCity();
+    }, [currZip]);
+    return (
+        <HStack space={1} alignItems={"center"}>
+            <SolInput
+                flex={1}
+                keyboardType={"number-pad"}
+                onChangeText={setCurrZip}
+            />
+            <Tag
+                pt={1}
+                flex={1}
+                h={"full"}
+                colorScheme={"muted"}
+                justifyContent={"flex-start"}
+                bg={"muted.200"}
+            >
+                {loading ? (
+                    <Center flex={1}>
+                        <Spinner colorScheme={"muted"} color={"muted.400"} />
+                    </Center>
+                ) : city ? (
+                    <Text fontWeight={500} fontSize={16}>
+                        {city.city}, {city.state}
+                    </Text>
+                ) : (
+                    <></>
+                )}
+            </Tag>
+        </HStack>
+    );
+};
+
+const LocationButton = () => {
+    const [loading, setLoading] = useState(false);
+
+    const getLocation = async () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    };
+    return (
+        <Button
+            onPress={getLocation}
+            borderRadius={0}
+            bg={"muted.200"}
+            color={"black"}
+            borderColor={"black"}
+            colorScheme={"muted"}
+            borderWidth={0}
+            _text={{ fontWeight: "medium", color: "black" }}
+            _pressed={{ bg: "muted.200" }}
+            leftIcon={<Icon as={Ionicons} name={"locate"} color={"black"} />}
+            isLoadingText={"Loading"}
+            isLoading={loading}
+        >
+            Add location
+        </Button>
     );
 };
 
