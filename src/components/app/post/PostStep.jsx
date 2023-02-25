81671b732bbdeae92c2c63240e17a6c396d59798
LeonJaggons <leonojaggon@gmail.com>
2023-02-25 11:17:07 -0500

    Add steps to routes, flags for done

--- src/components/app/post/PostStep.jsx
+++ src/components/app/post/PostStep.jsx
@@ -1,15 +1,15 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
-import { Box, Button, Heading, HStack, Icon } from "native-base";
+import { Box, Button, Divider, Heading, HStack, Icon } from "native-base";
 import BackButton from "../../util/BackButton";
 import { Ionicons } from "react-native-vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { indexOf } from "lodash";
 import { useSelector } from "react-redux";
-const PostStep = ({ title, children, canContinue }) => {
+const PostStep = ({ title, children, canContinue, final, noBack }) => {
     const postStep = useSelector((state) => state.post.postStep);
     const nav = useNavigation();
-    const steps = ["Post Images", "Main Details", "Price"];
+    const steps = ["Post Images", "Main Details", "Price", "Review", "Confirm"];
     const currI = indexOf(steps, title);
     const prevI = currI === 0 ? null : currI - 1;
     const nextI = currI === steps.length ? null : currI + 1;
@@ -18,9 +18,13 @@ const PostStep = ({ title, children, canContinue }) => {
         nextI && nav.navigate(steps[[nextI]]);
     };
     return (
-        <Box flex={1} safeArea p={2}>
-            <HStack alignItems={"center"} justifyContent={"space-between"}>
-                <Box opacity={prevI != null ? 1 : 0}>
+        <Box flex={1} safeArea>
+            <HStack
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                m={2}
+            >
+                <Box opacity={prevI != null && !noBack ? 1 : 0}>
                     <BackButton></BackButton>
                 </Box>
                 <Heading size={"md"}>{title}</Heading>
@@ -28,10 +32,11 @@ const PostStep = ({ title, children, canContinue }) => {
                     <BackButton></BackButton>
                 </Box>
             </HStack>
-            <Box mt={4} flex={1}>
+            <Box flex={1} m={final ? 0 : 2} mt={final ? 0 : 4}>
                 {children}
             </Box>
             <Button
+                m={2}
                 borderRadius={0}
                 colorScheme={"muted"}
                 bg={"black"}
@@ -40,7 +45,11 @@ const PostStep = ({ title, children, canContinue }) => {
                 isDisabled={!canContinue}
                 onPress={nextPage}
             >
-                Continue
+                {done
+                    ? "Return to Explore"
+                    : final
+                    ? "Post Listing"
+                    : "Continue"}
             </Button>
         </Box>
     );
