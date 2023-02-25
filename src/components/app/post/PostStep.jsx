20cb9091f834b61e4bff72ebf4f613dee34806ab
Leon Jaggon <leonojaggon@gmail.com>
2023-02-25 13:00:42 -0500

    Add post detail clearing on complete

--- src/components/app/post/PostStep.jsx
+++ src/components/app/post/PostStep.jsx
@@ -1,21 +1,49 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
-import { Box, Button, Divider, Heading, HStack, Icon } from "native-base";
+import {
+    Box,
+    Button,
+    Divider,
+    Heading,
+    HStack,
+    Icon,
+    ScrollView,
+} from "native-base";
 import BackButton from "../../util/BackButton";
 import { Ionicons } from "react-native-vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { indexOf } from "lodash";
-import { useSelector } from "react-redux";
-const PostStep = ({ title, children, canContinue, final, noBack }) => {
+import { useDispatch, useSelector } from "react-redux";
+const PostStep = ({ title, children, canContinue, final, noBack, done }) => {
     const postStep = useSelector((state) => state.post.postStep);
     const nav = useNavigation();
     const steps = ["Post Images", "Main Details", "Price", "Review", "Confirm"];
     const currI = indexOf(steps, title);
     const prevI = currI === 0 ? null : currI - 1;
     const nextI = currI === steps.length ? null : currI + 1;
+    const dispatch = useDispatch();
+    const isDone = done != null;
 
     const nextPage = () => {
-        nextI && nav.navigate(steps[[nextI]]);
+        if (isDone) {
+            dispatch({
+                type: "SET",
+                attr: "postCategory",
+                payload: null,
+            });
+            dispatch({
+                type: "SET",
+                attr: "postDetails",
+                payload: {},
+            });
+            nav.reset({
+                index: 0,
+                routes: [{ name: "Post Images" }],
+            });
+            nav.navigate("Explore");
+        } else {
+            nextI && nav.navigate(steps[[nextI]]);
+        }
     };
     return (
         <Box flex={1} safeArea>
@@ -33,7 +61,7 @@ const PostStep = ({ title, children, canContinue, final, noBack }) => {
                 </Box>
             </HStack>
             <Box flex={1} m={final ? 0 : 2} mt={final ? 0 : 4}>
-                {children}
+                <ScrollView>{children}</ScrollView>
             </Box>
             <Button
                 m={2}
@@ -45,7 +73,7 @@ const PostStep = ({ title, children, canContinue, final, noBack }) => {
                 isDisabled={!canContinue}
                 onPress={nextPage}
             >
-                {done
+                {isDone
                     ? "Return to Explore"
                     : final
                     ? "Post Listing"
