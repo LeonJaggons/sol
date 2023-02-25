2c56dfb2a367024f652ce626c6e0dfc9abce4a72
LeonJaggons <leonojaggon@gmail.com>
2023-02-25 11:16:41 -0500

    Add price, review, confirm to post nav

--- src/components/app/post/Post.jsx
+++ src/components/app/post/Post.jsx
@@ -1,23 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import TitleImgStep from "./TitleImgStep";
+import TitleImgStep from "./ImageStep";
 import MainDetailsStep from "./MainDetailsStep";
 import PriceStep from "./PriceStep";
+import ReviewStep from "./ReviewStep";
+import ConfirmStep from "./ConfirmStep";
 
 const Post = () => {
     const Stack = createStackNavigator();
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-                name="Post Images"
-                component={TitleImgStep}
-            ></Stack.Screen>
-            <Stack.Screen
-                name="Main Details"
-                component={MainDetailsStep}
-            ></Stack.Screen>
-            <Stack.Screen name="Price" component={PriceStep}></Stack.Screen>
+            <Stack.Screen name="Post Images" component={TitleImgStep} />
+            <Stack.Screen name="Main Details" component={MainDetailsStep} />
+            <Stack.Screen name="Price" component={PriceStep} />
+            <Stack.Screen name="Review" component={ReviewStep} />
+            <Stack.Screen name="Confirm" component={ConfirmStep} />
         </Stack.Navigator>
     );
 };
